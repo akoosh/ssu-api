c9195cf48f0487690e6650e9e195aef9ef96a24a
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-02-04 13:42:05 -0800

    Updated database.js to include the new models.

--- api/database.js
+++ api/database.js
@@ -1,9 +1,14 @@
 // database.js
 
 module.exports = function(mongoose) {
+    'use strict';
+
     mongoose.connect('mongodb://localhost/students');
 
     return {
-        Student: require('./models/student')(mongoose)
-    }
+        Student: require('./models/student')(mongoose),
+        Faculty: require('./models/faculty')(mongoose),
+        Course:  require('./models/course')(mongoose),
+        Class:   require('./models/class')(mongoose)
+    };
 };
