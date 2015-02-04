c115d80bbb307ef9540d1f743c85836662bae32d
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-02-04 11:42:30 -0800

    Made the student id field of student an index.

--- api/models/student.js
+++ api/models/student.js
@@ -4,7 +4,7 @@ module.exports = function(mongoose) {
     'use strict';
 
     return mongoose.model('Student', new mongoose.Schema({
-        sid: { type: String, required: true, unique: true },
+        sid: { type: String, required: true, unique: true, index: true },
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
         acadPlan: { type: String, required: true },
