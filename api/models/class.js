eb896bafb10127e8f7f8b2d5e1094c9f757ea2d4
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-02-22 15:11:58 -0800

    Modified the Class model and it's corresponding section in loadData() to now store meetings as subdocuments. This allows for one class to have multiple meeting times.

--- api/models/class.js
+++ api/models/class.js
@@ -13,14 +13,16 @@ module.exports = function(mongoose, plugins) {
         section: { type: String, required: true },
         component: { type: String, required: true },
         designation: { type: String, required: false },
-        facil_id: { type: String, required: false },
-        mtg_start: { type: String, required: true },
-        mtg_end: { type: String, required: true },
-        pat: { type: String, required: true }
+        meetings: [{
+            facil_id: { type: String, required: false },
+            mtg_start: { type: String, required: true },
+            mtg_end: { type: String, required: true },
+            pat: { type: String, required: true }
+        }]
     });
 
     // Cannot have the same class twice
-    schema.index({class_nbr: 1, term: 1, section: 1, mtg_start: 1, mtg_end: 1, pat: 1}, {unique: true});
+    schema.index({class_nbr: 1, term: 1}, {unique: true});
 
     plugins.forEach(function(plugin) {
         schema.plugin(plugin);
