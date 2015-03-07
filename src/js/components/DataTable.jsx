0ccf7a04b0d83f99f2f3019087de661fde830857
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-03-07 14:39:43 -0800

    Refactored the DataTable component to use reactabular. It currently supports searching, but no sorting.

--- src/js/components/DataTable.jsx
+++ src/js/components/DataTable.jsx
@@ -2,43 +2,63 @@
 
 var React = require('react');
 
+var Reactabular = require('reactabular');
+var Table       = Reactabular.Table;
+var Search      = Reactabular.Search;
+
+function nameFromKey(key) {
+    return key.replace(/_/g, ' ');
+}
+
 var DataTable = React.createClass({
+
+    getColumns: function(data) {
+        var columns = [];
+        if (data.length > 0) {
+            var keys = Object.keys(data[0]);
+            columns = keys.map(function(key) {
+                return {
+                    property: key,
+                    header: nameFromKey(key)
+                };
+            });
+        }
+
+        return columns;
+    },
+
+    getInitialState: function() {
+        return {
+            data: this.props.data,
+            columns: this.getColumns(this.props.data),
+            searchData: this.props.data
+        };
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.setState({
+            data: nextProps.data,
+            columns: this.getColumns(nextProps.data),
+            searchData: nextProps.data
+        });
+    },
 
     render: function() {
 
-        var rows = [];
-        var keys = [];
-
-        if (this.props.data.length > 0) {
-            keys = Object.keys(this.props.data[0]);
-            var tableHeadings = keys.map(function(key) {
-                return <th key={key}>{key}</th>;
-            });
-
-            rows.push(
-                <tr key='headings'>
-                    {tableHeadings}
-                </tr>
-            );
-        }
-
-        this.props.data.forEach(function(datum, i) {
-            var dataCells = keys.map(function(key) {
-                return <td key={key}>{datum[key]}</td>;
-            });
-
-            rows.push(
-                <tr key={'row_' + i}>
-                    {dataCells}
-                </tr>
-            );
-        });
+        var header = {
+            onClick: function(column) {
+                console.log(column);
+            }
+        };
 
         return (
             <div className='DataTable'>
-                <table>
-                    {rows}
-                </table>
+                <div className='SearchBar'>
+                    <span>Search: </span>
+                    <Search columns={this.state.columns} data={this.state.data} onResult={this.setState.bind(this)} />
+                </div>
+                <Table columns={this.state.columns} data={this.state.searchData} header={header} />
+                <span>{this.state.searchData.length} rows.</span>
             </div>
         );
     }
