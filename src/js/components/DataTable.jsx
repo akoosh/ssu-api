765bec68426ff8452f01bf12388647fda56b5af6
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-03-07 15:41:57 -0800

    Added pagification and sorting to the DataTable component.

--- src/js/components/DataTable.jsx
+++ src/js/components/DataTable.jsx
@@ -3,8 +3,10 @@
 var React = require('react');
 
 var Reactabular = require('reactabular');
+var Paginator   = require('react-pagify');
 var Table       = Reactabular.Table;
 var Search      = Reactabular.Search;
+var sortColumn  = Reactabular.sortColumn;
 
 function nameFromKey(key) {
     return key.replace(/_/g, ' ');
@@ -31,7 +33,11 @@ var DataTable = React.createClass({
         return {
             data: this.props.data,
             columns: this.getColumns(this.props.data),
-            searchData: this.props.data
+            searchData: this.props.data,
+            pagination: {
+                page: 0,
+                perPage: 10
+            }
         };
     },
 
@@ -43,22 +49,58 @@ var DataTable = React.createClass({
         });
     },
 
+    onSelect: function(page) {
+        var pagination = this.state.pagination || {};
+        pagination.page = page;
+
+        this.setState({
+            pagination: pagination
+        });
+    },
+
+    onPerPage: function(event) {
+        var pagination = this.state.pagination || {};
+        pagination.perPage = parseInt(event.target.value, 10);
+
+        this.setState({
+            pagination: pagination
+        });
+    },
+
     render: function() {
 
         var header = {
             onClick: function(column) {
-                console.log(column);
-            }
+                sortColumn(
+                    this.state.columns,
+                    column,
+                    this.state.searchData,
+                    this.setState.bind(this)
+                );
+            }.bind(this)
         };
+
+        var paginated = Paginator.paginate(this.state.searchData, this.state.pagination);
 
         return (
             <div className='DataTable'>
-                <div className='SearchBar'>
-                    <span>Search: </span>
+                <div className='table-controls'>
+                    <span> Per page: </span>
+                    <input type='text' defaultValue={this.state.pagination.perPage} onChange={this.onPerPage}></input>
+                    <span> Search: </span>
                     <Search columns={this.state.columns} data={this.state.data} onResult={this.setState.bind(this)} />
                 </div>
-                <Table columns={this.state.columns} data={this.state.searchData} header={header} />
-                <span>{this.state.searchData.length} rows.</span>
+                <Table columns={this.state.columns} data={paginated.data} header={header} />
+                <div className='pagination'>
+                    <span>Page: </span>
+                    <Paginator
+                        page={paginated.page}
+                        pages={paginated.amount}
+                        beginPages='3'
+                        endPages='3'
+                        onSelect={this.onSelect}
+                    />
+                </div>
             </div>
         );
     }
