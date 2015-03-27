bbbdfb4886c2aa5d6e3bced7604a8af913d3a6e4
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-03-26 18:39:02 -0700

    Added very basic form to upload student enrollment data.

--- src/js/components/controllerViews/FileUploadView.jsx
+++ src/js/components/controllerViews/FileUploadView.jsx
@@ -5,11 +5,38 @@ var Bootstrap  = require('react-bootstrap');
 var AppActions = require('../../actions/AppActions');
 
 var FileUploadView = React.createClass({
+
+    getInitialState: function() {
+        return {
+            isWaiting: false
+        };
+    },
+
+    onSubmit: function(event) {
+        event.preventDefault();
+
+        var formData = new FormData(event.target);
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', 'api/v0/update/csv', true);
+        xhr.onload = function(e) {
+            console.log(xhr);
+            this.setState({isWaiting: false});
+        }.bind(this);
+        xhr.send(formData);
+        this.setState({isWaiting: true});
+    },
+
     render: function() {
+        var waiting = this.state.isWaiting ? <span>Waiting...</span> : null;
         return (
             <div className='FileUploadView'>
                 <Bootstrap.PageHeader>File Upload</Bootstrap.PageHeader>
-                <Bootstrap.Input type="file" label="File" />
+                <form encType='multipart/form-data' method='POST' onSubmit={this.onSubmit}>
+                    <Bootstrap.Input type='file' name='file' label='Enrollment File'/>
+                    <Bootstrap.Input type='submit' value='Submit'/>
+                    {waiting}
+                </form>
             </div>
         );
     }
