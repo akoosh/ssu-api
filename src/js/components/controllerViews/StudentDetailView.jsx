d98a54acb71033738c97bfd8d90f1e97159ffdcf
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-06 16:18:56 -0700

    Added data formatting for the student detail page.

--- src/js/components/controllerViews/StudentDetailView.jsx
+++ src/js/components/controllerViews/StudentDetailView.jsx
@@ -3,12 +3,56 @@
 var React                   = require('react');
 var Router                  = require('react-router');
 var Bootstrap               = require('react-bootstrap');
+var _                       = require('lodash');
 var AppActions              = require('../../actions/AppActions');
 var DataTable               = require('../subviews/DataTable/DataTable');
 var StudentDataStore        = require('../../stores/StudentDataStore');
 
+function formattedName(obj) {
+    return [obj.last_name, obj.first_name].join(', ');
+}
+
+function advisorList(advisements) {
+    return advisements.map(function(advisement) {
+        return {
+            advisor: formattedName(advisement.advisor),
+            term: advisement.term_description,
+            academic_plan: advisement.acad_plan_descr
+        };
+    });
+}
+
+function classHistory(sections) {
+    var grouped = _.groupBy(sections, function(section) {
+        return section.class.term_description;
+    });
+
+    var history = {};
+    Object.keys(grouped).forEach(function(term) {
+        history[term] = grouped[term].map(function(section) {
+            return {
+                subject: section.class.course.subject,
+                catalog: section.class.course.catalog,
+                course_title: section.class.course.course_title,
+                instructor: formattedName(section.class.instructor),
+                units: section.class.class_units,
+                section: section.class.section,
+                component: section.class.component,
+                grade: section.grade
+            };
+        });
+    });
+
+    return history;
+}
+
 function getViewState() {
-    return StudentDataStore.getStudentData();
+    var data = StudentDataStore.getStudentData();
+    return {
+        student: data.student,
+        advisors: advisorList(data.advisors),
+        classHistory: classHistory(data.sections)
+    };
 }
 
 var StudentDetailView = React.createClass({
@@ -35,12 +79,35 @@ var StudentDetailView = React.createClass({
     render: function() {
         return (
             <div className='StudentDetailView'>
-                <Bootstrap.PageHeader>{this.state.student.first_name} {this.state.student.last_name}</Bootstrap.PageHeader>
-                <DataTable simple data={this.state.advisors}/>
-                <DataTable simple data={this.state.sections}/>
+                <Bootstrap.PageHeader>
+                    {this.state.student.first_name} {this.state.student.last_name} <small>{this.state.student.student_id}</small>
+                </Bootstrap.PageHeader>
+
+                <h2>Advisors</h2>
+                <Bootstrap.Row>
+                    <Bootstrap.Col xs={6}>
+                        <DataTable simple data={this.state.advisors}/>
+                    </Bootstrap.Col>
+                </Bootstrap.Row>
+
+                <Bootstrap.Row>
+                    <Bootstrap.Col xs={8}>
+                        <h2>Class History</h2>
+                        {Object.keys(this.state.classHistory).map(function(term) {
+                            return (
+                                <div key={term}>
+                                    <h4>{term}</h4>
+                                    <DataTable simple data={this.state.classHistory[term]}/>
+                                </div>
+                            );
+                        }.bind(this))}
+                    </Bootstrap.Col>
+                </Bootstrap.Row>
             </div>
         );
     }
 });
+
+// <DataTable simple data={this.state.sections}/>
 
 module.exports = StudentDetailView;
