ab4fc8297e343be2656bf82f77cb3f8543988623
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-05 21:44:54 -0700

    Replaced mockData.js with populateDatabase.js, which populates the database using the data files in the /data directory.

---
+++ populateDatabase.js
@@ -0,0 +1,41 @@
+// populateDatabase.js
+'use strict';
+
+var COURSES_PATH     = './data/all-courses.csv';
+var REQUISITES_PATH  = './data/cs-course-requisites.csv';
+var ENROLLMENTS_PATH = './data/cs-enrollments-2137-2153.csv';
+
+var mongoose = require('mongoose');
+var Async    = require('async');
+var db       = require('./api/database.js');
+
+mongoose.connect('mongodb://localhost/students', function(err) {
+    if (err) {
+        console.log('Connection error:', err);
+    } else {
+        Async.series([
+            function(callback) {
+                console.log('Processing courses...');
+                db.processFileWithSchema('courses', COURSES_PATH, callback);
+            },
+
+            function(callback) {
+                console.log('Processing requisites...');
+                db.processFileWithSchema('requisites', REQUISITES_PATH, callback);
+            },
+
+            function(callback) {
+                console.log('Processing enrollments...');
+                db.processFileWithSchema('enrollments', ENROLLMENTS_PATH, callback);
+            }
+        ], function(err) {
+            if (err) {
+                console.log('Error:', err);
+            } else {
+                console.log('Success!');
+            }
+
+            mongoose.disconnect();
+        });
+    }
+});
