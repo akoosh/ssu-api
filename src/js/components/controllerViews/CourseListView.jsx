7f54c463b589468b12517797f6ded381ec0daa19
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-06 16:18:26 -0700

    Moved data formatting for Courses to its own function.

--- src/js/components/controllerViews/CourseListView.jsx
+++ src/js/components/controllerViews/CourseListView.jsx
@@ -6,9 +6,21 @@ var AppActions              = require('../../actions/AppActions');
 var DataTable               = require('../subviews/DataTable/DataTable');
 var CourseListDataStore     = require('../../stores/CourseListDataStore');
 
+function formattedCourses(courses) {
+    return courses.map(function(course) {
+        var units = (course.min_units === course.max_units) ? course.min_units : course.min_units + '-' + course.max_units;
+        return {
+           subject: course.subject,
+           catalog: course.catalog,
+           course_title: course.course_title,
+           course_units: units
+        };
+    });
+}
+
 function getViewState() {
     return {
-        courses: CourseListDataStore.getCourses()
+        courses: formattedCourses(CourseListDataStore.getCourses())
     };
 }
 
@@ -32,20 +44,10 @@ var CourseListView = React.createClass({
     },
 
     render: function() {
-        var formattedCourses = this.state.courses.map(function(course) {
-            var units = (course.min_units === course.max_units) ? course.min_units : course.min_units + '-' + course.max_units;
-            return {
-               subject: course.subject,
-               catalog: course.catalog,
-               course_title: course.course_title,
-               course_units: units
-            };
-        });
-
         return (
             <div className='CourseListView'>
                 <Bootstrap.PageHeader>Courses</Bootstrap.PageHeader>
-                <DataTable data={formattedCourses}/>
+                <DataTable data={this.state.courses}/>
             </div>
         );
     }
