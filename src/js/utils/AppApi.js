ca33b6cc5ecb5b47df586f58bdca06d0a89bdf89
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-06 17:18:37 -0700

    Simplified callback logic in the API interface module.

--- src/js/utils/AppApi.js
+++ src/js/utils/AppApi.js
@@ -17,76 +17,34 @@ function makeApiRequest(url, callback) {
 var AppApi = {};
 
 AppApi.getStudents = function(callback) {
-    makeApiRequest('api/v0/students', function(err, students) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, students);
-        }
-    }.bind(this));
+    makeApiRequest('api/v0/students', callback);
 };
 
 AppApi.getStudentById = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id;
-    makeApiRequest(path, function(err, student) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, student);
-        }
-    }.bind(this));
+    makeApiRequest(path, callback);
 };
 
 AppApi.getAdvisorsByStudentId = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id + '/advisors';
-    makeApiRequest(path, function(err, advisors) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, advisors);
-        }
-    }.bind(this));
+    makeApiRequest(path, callback);
 };
 
 AppApi.getSectionsByStudentId = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id + '/classes';
-    makeApiRequest(path, function(err, sections) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, sections);
-        }
-    }.bind(this));
+    makeApiRequest(path, callback);
 };
 
 AppApi.getInstructors = function(callback) {
-    makeApiRequest('api/v0/instructors', function(err, instructors) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, instructors);
-        }
-    }.bind(this));
+    makeApiRequest('api/v0/instructors', callback);
 };
 
 AppApi.getAdvisors = function(callback) {
-    makeApiRequest('api/v0/advisors', function(err, advisors) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, advisors);
-        }
-    }.bind(this));
+    makeApiRequest('api/v0/advisors', callback);
 };
 
 AppApi.getCourses = function(callback) {
-    makeApiRequest('api/v0/courses', function(err, courses) {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, courses);
-        }
-    }.bind(this));
+    makeApiRequest('api/v0/courses', callback);
 };
 
 module.exports = AppApi;
