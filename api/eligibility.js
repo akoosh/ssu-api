9396e5068ef5c12b48095eb9d912c172b6684e90
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-09 12:51:21 -0700

    Added error handling to the eligibility function.

--- api/eligibility.js
+++ api/eligibility.js
@@ -27,51 +27,30 @@ function getEligibleStudentsBySubjectAndCatalogNumber(subject, catalog_number, c
     callback = (typeof callback === 'function') ? callback : function() {};
 
     // Get course for subject : catalog_number
-    models.Course.findOne({subject: subject.toUpperCase(), catalog: catalog_number}, function(err, course) {
+    db.getCourseBySubjectAndCatalogNumber(subject, catalog_number, objectHandler(callback, function(course) {
 
         // Get the req. for the given subject : catalog_number
-        models.Requisite.find( { course : course._id }, function(err, requisites) {
-            var preRequisites = requisites.filter( function(requisite){ 
-              return requisite.type === 'P';
-            });
+        models.Requisite.find( { course : course._id }, arrayHandler(callback, function(requisites) {
 
-            var reqStudentIdGetters = preRequisites.map( function(prerequisite) {
-                return function(preCallback) {
-                    // Find all section sections that are instances of the prerequisite
-                    models.Section.find( { course : prerequisite.requisite }, function(err, sections) {
-                        var sectionIds = _.pluck( sections, '_id' );
-
-                        // Find all students that have taken one of those sections
-                        models.Enrollment.find( { section : { $in : sectionIds } }, function(err, enrollments){
-                            // Filter out failing grades
-                            var passing = enrollments.filter(function(enrollment) {
-                                return (gradePoints[enrollment.grade] || 0) >= (gradePoints[prerequisite.grade] || 0);
-                            });
-
-                            var studentIds = _.uniq( _.pluck( passing, 'student' ).map(String) );
-                            preCallback( null, studentIds );
-                        });
-                    });
-                };
-            });
+            var eligibleStudentIdGetters = requisites.filter(prerequisiteFilter).map(eligibleStudentIdGetterByPrerequisite);
 
             Async.parallel({
                 // all students that have fullfilled prerequisites
                 eligibleStudentIds: function(callback) {
-                    Async.parallel( reqStudentIdGetters, function(err,results) {
-                        // Produces all of the studentIds who are eligible to take the section
+                    Async.parallel( eligibleStudentIdGetters, arrayHandler(callback, function(results) {
+                        // Produces all of the studentIds who are eligible to take the course
                         var elgStudents = _.spread( _.intersection)(results);
                         callback(null, elgStudents);
-                    });
+                    }));
                 },
 
                 // all students that have already passed the course
                 doneStudentIds: function(callback) {
-                    models.Section.find( { course : course._id }, function(err, sections) {
+                    models.Section.find( { course : course._id }, arrayHandler(callback, function(sections) {
                         var sectionIds = _.pluck( sections, '_id' );
 
                         // Find all students that have taken one of those sections
-                        models.Enrollment.find( { section : { $in : sectionIds } }, function(err, enrollments){
+                        models.Enrollment.find( { section : { $in : sectionIds } }, arrayHandler(callback, function(enrollments){
                             // Filter out failing grades
                             var passing = enrollments.filter(function(enrollment) {
                                 return (gradePoints[enrollment.grade] || 0) >= gradePoints['C-'];
@@ -79,19 +58,42 @@ function getEligibleStudentsBySubjectAndCatalogNumber(subject, catalog_number, c
 
                             var studentIds = _.uniq( _.pluck( passing, 'student' ).map(String) );
                             callback( null, studentIds );
-                        });
-                    });
+                        }));
+                    }));
                 }
 
-            }, function(err, results) {
+            }, arrayHandler(callback, function(results) {
                 // Produces the list of students that are eligible to take the course excluding those
                 // that have already taken and passed it.
                 var studentIds = _.difference(results.eligibleStudentIds.map(String), results.doneStudentIds.map(String));
                 models.Student.find( { _id : { $in : studentIds } }, callback );
-            });
+            }));
+        }));
+    }));
+}
 
-        });
-    });
+function prerequisiteFilter(requisite) {
+    return requisite.type === 'P';
+}
+
+function eligibleStudentIdGetterByPrerequisite(prerequisite) {
+    return function(callback) {
+        // Find all section sections that are instances of the prerequisite
+        models.Section.find( { course : prerequisite.requisite }, arrayHandler(callback, function(sections) {
+            var sectionIds = _.pluck( sections, '_id' );
+
+            // Find all students that have taken one of those sections
+            models.Enrollment.find( { section : { $in : sectionIds } }, arrayHandler(callback, function(enrollments){
+                // Filter out failing grades
+                var passing = enrollments.filter(function(enrollment) {
+                    return (gradePoints[enrollment.grade] || -1) >= (gradePoints[prerequisite.grade] || 0);
+                });
+
+                var studentIds = _.uniq( _.pluck( passing, 'student' ).map(String) );
+                callback( null, studentIds );
+            }));
+        }));
+    };
 }
 
 module.exports = {
