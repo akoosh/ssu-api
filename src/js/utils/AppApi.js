75d7eb1b1aa69bfc259cedaa77e5b2ee68b92c70
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-19 16:16:43 -0700

    Got rid of jQuery. Resolves #44.

--- src/js/utils/AppApi.js
+++ src/js/utils/AppApi.js
@@ -1,25 +1,24 @@
 // AppApi.js
 'use strict';
 
-var $ = require('jquery');
+function makeApiGetRequest(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
 
-function makeApiRequest(url, callback) {
-    $.ajax({
-        url: url,
-        dataType: 'json',
-        success: function(data, status) {
-            // Return [] in the case that 'nocontent' status is received.
-            if (status === 'nocontent') {
-                callback(null, []);
-            } else {
-                callback(null, data);
-            }
-        },
-        error: function(xhr, status, err) {
-            console.error(url, status, err.toString());
-            callback(err);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+            callback(null, data || []);
+        } else {
+            callback('server error');
         }
-    });
+    };
+
+    request.onerror = function() {
+        callback('xhr error');
+    };
+
+    request.send();
 }
 
 var AppApi = {};
@@ -27,82 +26,82 @@ var AppApi = {};
 // Student
 
 AppApi.getStudents = function(callback) {
-    makeApiRequest('api/v0/students', callback);
+    makeApiGetRequest('api/v0/students', callback);
 };
 
 AppApi.getStudentById = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id;
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getAdvisorsByStudentId = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id + '/advisors';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getSectionsByStudentId = function(student_id, callback) {
     var path = 'api/v0/students/' + student_id + '/sections';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 // Instructor
 
 AppApi.getInstructors = function(callback) {
-    makeApiRequest('api/v0/instructors', callback);
+    makeApiGetRequest('api/v0/instructors', callback);
 };
 
 AppApi.getInstructorById = function(instructor_id, callback) {
     var path = 'api/v0/instructors/' + instructor_id;
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getSectionsByInstructorId = function(instructor_id, callback) {
     var path = 'api/v0/instructors/' + instructor_id + '/sections';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 // Advisor
 
 AppApi.getAdvisors = function(callback) {
-    makeApiRequest('api/v0/advisors', callback);
+    makeApiGetRequest('api/v0/advisors', callback);
 };
 
 AppApi.getAdvisorById = function(advisor_id, callback) {
     var path = 'api/v0/advisors/' + advisor_id;
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getStudentsByAdvisorId = function(advisor_id, callback) {
     var path = 'api/v0/advisors/' + advisor_id + '/students';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 // Course
 
 AppApi.getCourses = function(callback) {
-    makeApiRequest('api/v0/courses', callback);
+    makeApiGetRequest('api/v0/courses', callback);
 };
 
 AppApi.getCourseBySubjectAndCatalogNumber = function(subject, catalog_number, callback) {
     var path = 'api/v0/courses/subjects/' + subject + '/' + catalog_number;
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getSectionsBySubjectAndCatalogNumber = function(subject, catalog_number, callback) {
     var path = 'api/v0/courses/subjects/' + subject + '/' + catalog_number + '/sections';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 // Section
 
 AppApi.getSectionByTermAndClassNumber = function(term, class_nbr, callback) {
     var path = 'api/v0/sections/terms/' + term + '/' + class_nbr;
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 AppApi.getStudentsInSectionByTermAndClassNumber = function(term, class_nbr, callback) {
     var path = 'api/v0/sections/terms/' + term + '/' + class_nbr + '/students';
-    makeApiRequest(path, callback);
+    makeApiGetRequest(path, callback);
 };
 
 module.exports = AppApi;
