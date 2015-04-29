b6adb894e3ac514eb28c108430a515b3ae773293
Kyle Janssen <kyle.janssen.0@gmail.com>
2015-04-29 11:24:09 -0700

    Made the courses in the department detail view link to the course detail view.

--- src/js/components/controllerViews/detailViews/DepartmentDetailView.jsx
+++ src/js/components/controllerViews/detailViews/DepartmentDetailView.jsx
@@ -5,9 +5,9 @@ var React                   = require('react');
 var Router                  = require('react-router');
 var Bootstrap               = require('react-bootstrap');
 var _                       = require('lodash');
-var AppActions              = require('../../../actions/AppActions');
 var DataTable               = require('../../subviews/DataTable/DataTable');
-var CourseDataStore     = require('../../../stores/CourseDataStore');
+var AppActions              = require('../../../actions/AppActions');
+var CourseDataStore         = require('../../../stores/CourseDataStore');
 
 function formattedCourses(courses) {
     return courses.map(function(course) {
@@ -48,11 +48,15 @@ var DepartmentDetailView = React.createClass({
         CourseDataStore.removeChangeListener(this.onChange);
     },
 
+    onCourseClick: function(course) {
+        this.transitionTo('course-detail', {subject: course.subject, catalog_number: course.catalog});
+    },
+
     render: function() {
         return (
             <div className='DepartmentDetailView'>
                 <Bootstrap.PageHeader>{this.getParams().subject} Department</Bootstrap.PageHeader>
-                <DataTable simple data={this.state.courses}/>
+                <DataTable simple clickable data={this.state.courses} onRowClick={this.onCourseClick}/>
             </div>
         );
     }
